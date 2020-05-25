fc1ccc084bac706f8a42cbd3b98ecc2cf4fda80d
Ihor Podpriadok <podpryadok@gmail.com>
2020-05-25 20:58:29 +0300

    Git Init

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Learning_MVC.Data;
+using Learning_MVC.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Learning_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Movie MVC
+This tutorial project was been take from https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/?view=aspnetcore-3.1
+
+In this tutorial I learned ASP.NET Core MVC web development with controllers and views.
